dd75af739aee8b2c2c2c0166b9a5ec6a18065d55
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-02-28 16:25:30 -0600

    Initial Commit

---
+++ CoreMLExperiments/CoreMLExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLExperimentsApp.swift
+//  CoreMLExperiments
+//
+//  Created by Andrew Montgomery on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
